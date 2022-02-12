d9069f679c1638f4ca979478a00c833479221af8
sfaut <temposeb@free.fr>
2022-02-12 23:39:33 +0100

    Csv\Writer::all() creation

--- src/Writer.php
+++ src/Writer.php
@@ -1,3 +1,19 @@
 <?php
 
-// Not implemented yet
+namespace sfaut\Csv;
+
+class Writer
+{
+    public static function all (string $file, array $records, array $parameters = [])
+    {
+        $separator = $parameters['separator'] ?? ',';
+        $enclosure = $parameters['enclosure'] ?? '"';
+        $escape = $parameters['escape'] ?? '';
+
+        $stream = fopen($file, 'w');
+
+        foreach ($records as $record) {
+            fputcsv($stream, $record, $separator, $enclosure, $escape);
+        }
+    }
+}
